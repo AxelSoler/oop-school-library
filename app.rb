6c12d8382aef721df7c651efd40f7b015d4ab1db
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-07-19 17:00:06 -0300

    create a person methods

--- app.rb
+++ app.rb
@@ -26,8 +26,41 @@ class App
     list_menu
   end
 
-  def create_person(person)
-    Person.new(person)
+  def create_person
+    puts 'Do you want to create a student (1) or teacher (2)? [Input the number]: '
+    selected = gets.chomp
+    case selected
+    when '1' then create_student
+    when '2' then create_teacher
+    else
+      puts 'wrong number, select student (1) or teacher (2)'
+      create_person
+    end
+    list_menu
+  end
+
+  def create_student
+    puts 'Age: '
+    selected_age = gets.chomp
+    puts 'Name: '
+    selected_name = gets.chomp
+    puts 'Has parent permission? [Y/N]: '
+    selected_permission = gets.chomp.downcase == 'y'
+    student = Student.new(selected_name, selected_age, selected_permission)
+    @persons.push(student)
+    puts 'Person created successfully'
+  end
+
+  def create_teacher
+    puts 'Age: '
+    selected_age = gets.chomp
+    puts 'Name: '
+    selected_name = gets.chomp
+    puts 'Specialization: '
+    selected_specialization = gets.chomp
+    teacher = Teacher.new(selected_name, selected_age, selected_specialization)
+    @persons.push(teacher)
+    puts 'Person created successfully'
   end
 
   def create_book(book)
